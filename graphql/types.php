b733ff2749ad153bdd53935c7c2f1c1cd937e864
Waneyscz <loayzaclaros@gmail.com>
2022-06-29 18:08:44 -0400

    tipos

--- graphql/types.php
+++ graphql/types.php
@@ -12,4 +12,59 @@ $PlayListType=new ObjectType([
         'Prioridad'=>Type::int()
     ]
 ]);
+$CancionPlayListType=new ObjectType([
+    'name' => 'CancionPlayListType',
+    'description' => 'CancionPlayListType',
+    'fields'=>[
+        'ID'=>Type::int(),
+        'Cancion'=>Type::int(),
+        'PlayList'=>Type::int()
+    ]
+]);
+$CancionType=new ObjectType([
+    'name' => 'CancionType',
+    'description' => 'CancionType',
+    'fields'=>[
+        'ID'=>Type::int(),
+        'Nombre'=>Type::string(),
+        'Imagen'=>Type::int(),
+        'Pista'=>Type::string(),
+        'Url_Cancion'=>Type::string(),
+        'Conteo'=>Type::int(),
+        'Letra'=>Type::string(),
+        'Album'=>Type::string()
+    ]
+]);
+
+$FotoType=new ObjectType([
+    'name' => 'FotoType',
+    'description' => 'FotoType',
+    'fields'=>[
+        'ID'=>Type::int(),
+        'Url'=>Type::string()
+        
+    ]
+]);
+$AlbumType=new ObjectType([
+    'name' => 'AlbumType',
+    'description' => 'AlbumType',
+    'fields'=>[
+        'ID'=>Type::int(),
+        'Gesion'=>Type::string(),
+        'Foto'=>Type::int(),
+        'Artista'=>Type::string()
+        
+    ]
+]);
+$ArtistaType=new ObjectType([
+    'name' => 'ArtistaType',
+    'description' => 'ArtistaType',
+    'fields'=>[
+        'ID'=>Type::int(),
+        'Nombre'=>Type::string(),
+        'Alias'=>Type::string(),
+        'Foto'=>Type::int()
+    ]
+]);
+
 ?>
